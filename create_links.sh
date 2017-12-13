717d024d7c8bd8bb42cdaad4b95e64bb7ebec36b
Martin Bertsche <martin.bertsche@winterhalter.de>
2017-12-13 07:35:00 +0100

    now removing directories and files before creating links

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+rm -f ~/.zshrc
 ln -sf $(pwd)/zshrc ~/.zshrc
+rm -f ~/.vimrc
 ln -sf $(pwd)/vimrc ~/.vimrc
+rm -rf ~/.vim
 ln -sf $(pwd)/vim ~/.vim
+rm -f ~/.profile
 ln -sf $(pwd)/profile ~/.profile
+rm -f ~/.tmumx.comf
 ln -sf $(pwd)/tmux.conf ~/.tmux.conf
