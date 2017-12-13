cab3622c80a31254c8e3ed600e5bef7b65fededd
Martin Bertsche <martin@martinbertsche.com>
2017-12-13 21:12:31 +0100

    work on init script

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,52 @@
 #! /bin/bash
 
-ln -sf $(pwd)/zshrc ~/.zshrc
-ln -sf $(pwd)/vimrc ~/.vimrc
-ln -sf $(pwd)/vim ~/.vim
-ln -sf $(pwd)/profile ~/.profile
-ln -sf $(pwd)/tmux.conf ~/.tmux.conf
+PACKAGES="
+zsh
+tmux
+vim-nox
+realpath
+python3-pip
+"
+
+PYTHON_PACKAGES="
+powerline-status
+"
+
+REPLACEMENTS="
+vim
+zshrc
+vimrc
+profile
+tmux.conf
+"
+
+echo "Installing required packages..."
+sudo apt-get install ${PACKAGES} &> /dev/null
+
+SCRIPT="$(realpath $0)"
+SCRIPTPATH="$(dirname $SCRIPT)"
+echo "Script location: ${SCRIPTPATH}"
+
+echo "Installing powerline..."
+pip3 install --user ${PYTHON_PACKAGES} &> /dev/null
+
+echo "Removing files and folders to be replaced..."
+for FIL in ${REPLACEMENTS}
+    do
+        RECURSIVE=
+        DESTINATION="~/.${FIL}"
+        SOURCE="${SCRIPTPATH}/${FIL}"
+ 
+        if [ -d "${SOURCE}" ]
+            then
+                RECURSIVE=r
+        fi
+
+        echo "Removing existing file or directory: ${DESTINATION}"
+        rm -${RECURSIVE}f "${DESTINATION}"
+
+        echo "Creating symbolic link: ${DESTINATION} -> ${SOURCE}"
+        ln -sf "${DESTINATION}" "${SOURCE}"
+
+    done
+
