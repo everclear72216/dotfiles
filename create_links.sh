fd65e5a5400bb88ea4ddd8609a3624f7941d0fd4
Martin Bertsche <martin@martinbertsche.com>
2018-01-29 01:13:22 +0100

    added ranger config

--- create_links.sh
+++ create_links.sh
@@ -25,6 +25,7 @@ tmux.conf
 config/i3/config
 config/powerline
 powerline-shell.json
+config/ranger/rc.conf
 config/i3status/config
 local/bin/stterm_launcher
 local/share/applications/simple-terminal.desktop
