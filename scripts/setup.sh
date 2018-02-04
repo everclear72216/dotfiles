aa0b811eeb51c940e42b14244f54189f66018015
Martin Bertsche <martin@martinbertsche.com>
2018-02-04 18:30:05 +0100

    finished partitioning scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,24 @@
 #! /bin/sh
 
+echo "Setting up your config files..."
+echo ""
 
+set -e
+
+echo "Installing required deb-packages..."
+
+./install_deb.sh
+
+echo "Installing required pip-packages..."
+
+./install_python.sh
+
+echo "Setting up symlinks..."
+./create_links.sh
+
+echo "Configuring environment..."
+
+./do_configure.sh
+
+echo "Done!"
+echo ""
