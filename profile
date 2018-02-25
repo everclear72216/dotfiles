3f3dc1c680e8d7d2ed55ce18848cdf3c12a5eb0f
Martin Bertsche <martin72216@googlemail.com>
2018-02-25 14:31:10 +0100

    Osome changes to support debian

--- profile
+++ profile
@@ -3,5 +3,6 @@ if [ -d "$HOME/.local/bin" ]
 	PATH="$HOME/.local/bin:$PATH"
 fi
 
+export SHELL=/bin/zsh
 export TERMINAL=stterm_launcher
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,12 +8,12 @@ SCRIPTPATH="$(dirname $SCRIPT)"
 
 set -e
 
-echo "Installing required deb-packages..."
-. ${SCRIPTPATH}/install_deb.sh
-wait
+#echo "Installing required deb-packages..."
+#. ${SCRIPTPATH}/install_deb.sh
+#wait
 
-echo "Installing required pip-packages..."
-. ${SCRIPTPATH}/install_python.sh
+#echo "Installing required pip-packages..."
+#. ${SCRIPTPATH}/install_python.sh
 
 echo "Setting up symlinks..."
 . ${SCRIPTPATH}/create_links.sh
